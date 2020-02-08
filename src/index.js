e9cdaa352ac0309779fc03eeb557669d4243db92
toda93 <dttong93@gmail.com>
2020-02-08 14:29:20 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,4 +1,113 @@
-import RedisAsync from './RedisAsync';
+import redis from 'redis';
+import {promisify} from 'util';
 
 
-export default RedisAsync
+class RedisAsync {
+    constructor(host, port) {
+        this.connected = false;
+        this.host = host;
+        this.port = port;
+        this.connect();
+    }
+
+    connect() {
+        this.client = redis.createClient({
+            host: this.host,
+            port: this.port,
+
+            retry_strategy: (options) => {
+                this.connected = false;
+                this._alert('Redis disconnected');
+                this.client.quit();
+            }
+        });
+
+        this.client.client('SETNAME', process.env.PROJECT);
+
+        this.client.on('connect', () => {
+            this.connected = true;
+            this._alert('Redis connected');
+        });
+
+        this.client.on('end', () => {
+            this._alert('Redis end');
+        });
+
+        this.client.on('error',  (err) => {
+            this._alert('Redis Error' + err);
+        });
+    }
+
+    async get(key) {
+        if (this.connected) {
+
+            console.log(`Redis GET ${key}`);
+
+            const getAsync = promisify(this.client.get).bind(this.client);
+            const data = await getAsync(key);
+            return JSON.parse(data);
+        } else {
+            this.connect();
+        }
+
+        return null;
+    }
+
+    async ttl(key) {
+        const ttlAsync = promisify(this.client.ttl).bind(this.client);
+        return await ttlAsync(key);
+    }
+
+    async expire(key, time = 100000) {
+        this.client.expire(key, time);
+    }
+
+    async set(key, data, time = 100000) {
+        if (this.connected) {
+
+            console.log(`Redis SET ${key}`);
+
+            await this.client.set(key, JSON.stringify(data), 'EX', time);
+        } else {
+            this.connect();
+        }
+    }
+
+    async remove(key, exact) {
+
+        if (this.connected) {
+
+            if (exact) {
+                console.log(`Redis REMOVE ${key}`);
+
+                await this.client.del(key);
+            } else {
+                key = '*' + key + '*';
+
+                const getAsync = promisify(this.client.keys).bind(this.client);
+                const keys = await getAsync(key);
+
+                if (keys.length > 0) {
+                    console.log(`Redis REMOVE keys`, keys);
+                    await this.client.del(keys);
+                }
+            }
+
+        } else {
+            this.connect();
+        }
+    }
+
+
+    setAlertCallback(callback) {
+        this.alertCallback = callback;
+    }
+
+    _alert(msg) {
+        if (typeof this.alertCallback === 'function') {
+            this.alertCallback(msg);
+        }
+    }
+}
+
+export default RedisAsync;
